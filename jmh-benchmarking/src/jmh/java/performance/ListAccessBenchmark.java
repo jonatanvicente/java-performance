99cbbae77c2b4f8fada553a2449324f9a10da644
Jonatan Vicente <it@jonatanvicente.com>
2025-07-01 10:40:16 +0200

    Added benchmarktest comparing arraylist vs linkedlist

---
+++ jmh-benchmarking/src/jmh/java/performance/ListAccessBenchmark.java
@@ -0,0 +1,44 @@
+package performance;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+@Fork(1)
+@Warmup(iterations = 0)
+@Measurement(iterations = 1)
+public class ListAccessBenchmark {
+
+    //@Param({"1000", "10000", "100000"})
+    @Param({"100000"})
+    private int size;
+
+    private List<Integer> arrayList;
+    private List<Integer> linkedList;
+
+    @Setup(Level.Iteration)
+    public void setup() {
+        arrayList = new ArrayList<>();
+        linkedList = new LinkedList<>();
+
+        for (int i = 0; i < size; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+    }
+
+    @Benchmark
+    public int accessArrayListMiddle() {
+        return arrayList.get(size / 2);
+    }
+
+    @Benchmark
+    public int accessLinkedListMiddle() {
+        return linkedList.get(size / 2);
+    }
+}
